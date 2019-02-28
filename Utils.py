d17a5ec7bf60cd509a554f2e6f6e3d2691db56ab
Albert <albert.garcia.sanchez96@gmail.com>
2019-02-28 19:23:23 +0100

    Read implemented

---
+++ Utils.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+def readData(filePath):
+    file = open(filePath, 'r')
+    line = file.readline().split(" ")
+    rows = int(line[0])
+    
+    photos = np.empty((rows), dtype=dict)
+    for i in range(rows):
+        line = file.readline()
+        values = line.split()
+        dicc = { 'orientation' : values[0] }
+        tags = np.array([values[i] for i in range(2, 2+int(values[1]))], dtype=str)
+        dicc['tags'] = tags
+        photos[i] = dicc
+
+    file.close()
+
+    return photos
+
+
+def writeOutput(result, problemTitle):
+    file = open(problemTitle+'_result.out', 'w')
+
+    file.write(str(len(result))+'\n')
+    # TODO write result
+
+    file.close()
+
+
+data = readData('a_example.txt')
+print(data)
+
+
